54e75794b5747d02e94017bdd889fdd2c9ce9bd9
Dan Hodos <danhodos@gmail.com>
2016-01-13 13:17:38 -0600

    Initial creation of AlamofireGenome framework

---
+++ AlamofireGenome/AlamofireGenome.h
@@ -0,0 +1,19 @@
+//
+//  AlamofireGenome.h
+//  AlamofireGenome
+//
+//  Created by Daniel Hodos on 1/13/16.
+//  Copyright © 2016 Table XI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlamofireGenome.
+FOUNDATION_EXPORT double AlamofireGenomeVersionNumber;
+
+//! Project version string for AlamofireGenome.
+FOUNDATION_EXPORT const unsigned char AlamofireGenomeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireGenome/PublicHeader.h>
+
+
